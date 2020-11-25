4bb5cc8ea44ced85f5a77fca30317ab417d71459
Corentin Crébier <crebier.corentin@gmail.com>
2020-11-25 15:26:05 +0100

    Add bookmark/:id route

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 // app.js
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const connection = require('./connection');
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const connection = require("./connection");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (_, res) => res.json({ message: 'Hello World!' }));
+app.get("/", (_, res) => res.json({ message: "Hello World!" }));
 
-app.post('/bookmarks', (req, res) => {
+app.post("/bookmarks", (req, res) => {
   const { url, title } = req.body;
   if (!url || !title) {
-    return res.status(422).json({ error: 'required field(s) missing' });
+    return res.status(422).json({ error: "required field(s) missing" });
   }
-  connection.query('INSERT INTO bookmark SET ?', req.body, (err, stats) => {
+  connection.query("INSERT INTO bookmark SET ?", req.body, (err, stats) => {
     if (err) return res.status(500).json({ error: err.message, sql: err.sql });
 
-    connection.query('SELECT * FROM bookmark WHERE id = ?', stats.insertId, (err, records) => {
-      if (err) return res.status(500).json({ error: err.message, sql: err.sql });
-      return res.status(201).json(records[0]);
-    });
+    connection.query(
+      "SELECT * FROM bookmark WHERE id = ?",
+      stats.insertId,
+      (err, records) => {
+        if (err)
+          return res.status(500).json({ error: err.message, sql: err.sql });
+        return res.status(201).json(records[0]);
+      }
+    );
   });
+});
+
+app.get("/bookmarks/:id", (req, res) => {
+  const { id } = req.params;
+
+  connection.query(
+    "SELECT * FROM bookmark WHERE id = ?",
+    [id],
+    (err, results) => {
+      if (err) {
+        return res.sendStatus(500);
+      }
+
+      if (results.length === 0) {
+        return res.status(404).json({ error: "Bookmark not found" });
+      }
+
+      return res.json(results[0]);
+    }
+  );
 });
 
 module.exports = app;
--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -1,45 +1,75 @@
-
 // test/app.integration.spec.js
-const request = require('supertest');
-const app = require('../app');
+const request = require("supertest");
+const app = require("../app");
+const connection = require("../connection");
 
-describe('Test routes', () => {
+describe("Test routes", () => {
+  beforeEach((done) => connection.query("TRUNCATE bookmark", done));
+
+  afterAll((done) => connection.end(done));
   it('GET / sends "Hello World" as json', (done) => {
     request(app)
-      .get('/')
+      .get("/")
       .expect(200)
-      .expect('Content-Type', /json/)
-      .then(response => {
-        const expected = { message: 'Hello World!'};
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        const expected = { message: "Hello World!" };
         expect(response.body).toEqual(expected);
         done();
       });
   });
 
-  it('POST /bookmarks - error (fields missing) ', (done) => {
+  it("POST /bookmarks - error (fields missing)", (done) => {
     request(app)
-      .post('/bookmarks')
+      .post("/bookmarks")
       .send({})
       .expect(422)
-      .expect('Content-Type', /json/)
-      .then(response => {
-        const expected = { error: 'required field(s) missing' };
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        const expected = { error: "required field(s) missing" };
         expect(response.body).toEqual(expected);
         done();
       });
   });
 
-  it('POST /bookmarks - OK (fields provided) ', (done) => {
+  it("POST /bookmarks - OK (fields provided)", (done) => {
     request(app)
-      .post('/bookmarks')
-      .send({ url: 'https://jestjs.io', title: 'Jest' })
+      .post("/bookmarks")
+      .send({ url: "https://jestjs.io", title: "Jest" })
       .expect(201)
-      .expect('Content-Type', /json/)
-      .then(response => {
-        const expected = { id: 1, url: 'https://jestjs.io', title: 'Jest' };
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        const expected = { id: 1, url: "https://jestjs.io", title: "Jest" };
         expect(response.body).toEqual(expected);
         done();
       })
       .catch(done);
+  });
+
+  describe("GET /bookmarks/:id", () => {
+    const testBookmark = { url: "https://nodejs.org/", title: "Node.js" };
+    beforeEach((done) =>
+      connection.query("TRUNCATE bookmark", () =>
+        connection.query("INSERT INTO bookmark SET ?", testBookmark, done)
+      )
+    );
+
+    it("GET /bookmarks/:id - error (id doesn't exist)", async () => {
+      const res = await request(app)
+        .get("/bookmarks/404")
+        .expect(404)
+        .expect("Content-Type", /json/);
+
+      expect(res.body).toEqual({ error: "Bookmark not found" });
+    });
+
+    it("GET /bookmarks/:id - OK (id exists)", async () => {
+      const res = await request(app)
+        .get("/bookmarks/1")
+        .expect(200)
+        .expect("Content-Type", /json/);
+
+      expect(res.body).toEqual({ id: 1, ...testBookmark });
+    });
   });
 });
